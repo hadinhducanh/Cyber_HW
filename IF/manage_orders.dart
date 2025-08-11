cd609e306d44953dd4da41dc62e411fc1e3a3ef9
hadinhducanh <ducanhhadinh@gmail.com>
2025-08-11 14:18:04 +0700

    add loop

--- IF/manage_orders.dart
+++ IF/manage_orders.dart
@@ -1,36 +1,44 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Input product name: ');
-  String productName = stdin.readLineSync() ?? '';
+  while (true) {
+    stdout.write('Input product name: ');
+    String productName = stdin.readLineSync() ?? '';
 
-  stdout.write('Input quantity: ');
-  int quantity = int.parse(stdin.readLineSync() ?? '0');
+    stdout.write('Input quantity: ');
+    int quantity = int.parse(stdin.readLineSync() ?? '0');
 
-  stdout.write('Input unit price: ');
-  double unitPrice = double.parse(stdin.readLineSync() ?? '0');
+    stdout.write('Input unit price: ');
+    double unitPrice = double.parse(stdin.readLineSync() ?? '0');
 
-  double totalPrice = quantity * unitPrice;
+    double totalPrice = quantity * unitPrice;
 
-  double discount = 0;
-  if (totalPrice >= 1000000) {
-    discount = totalPrice * 0.1;
-  } else if (totalPrice >= 500000) {
-    discount = totalPrice * 0.05;
+    double discount = 0;
+    if (totalPrice >= 1000000) {
+      discount = totalPrice * 0.1;
+    } else if (totalPrice >= 500000) {
+      discount = totalPrice * 0.05;
+    }
+
+    double amountAfterDiscount = totalPrice - discount;
+
+    double vat = amountAfterDiscount * 0.08;
+
+    double totalPayment = amountAfterDiscount + vat;
+
+    print('\n--- INVOICE ---');
+    print('Product Name: $productName');
+    print('Quantity: $quantity');
+    print('Unit Price: ${unitPrice.toStringAsFixed(0)}');
+    print('Total Price: ${totalPrice.toStringAsFixed(0)}');
+    print('Discount: ${discount.toStringAsFixed(0)}');
+    print('VAT (8%): ${vat.toStringAsFixed(0)}');
+    print('Total Payment: ${totalPayment.toStringAsFixed(0)}');
+
+    stdout.write('Do you want to add another product? (y/n): ');
+    String? choice = stdin.readLineSync();
+    if (choice?.toLowerCase() != 'y') {
+      break;
+    }
   }
-
-  double amountAfterDiscount = totalPrice - discount;
-
-  double vat = amountAfterDiscount * 0.08;
-
-  double totalPayment = amountAfterDiscount + vat;
-
-  print('\n--- INVOICE ---');
-  print('Product Name: $productName');
-  print('Quantity: $quantity');
-  print('Unit Price: ${unitPrice.toStringAsFixed(0)}');
-  print('Total Price: ${totalPrice.toStringAsFixed(0)}');
-  print('Discount: ${discount.toStringAsFixed(0)}');
-  print('VAT (8%): ${vat.toStringAsFixed(0)}');
-  print('Total Payment: ${totalPayment.toStringAsFixed(0)}');
 }
